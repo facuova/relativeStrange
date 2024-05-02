05987ea859db37cb0447b61816ffed99b4d7c3ac
Facu <facuovalles@gmail.com>
2024-05-02 10:26:10 -0300

    Función para transformar xlsx en df

---
+++ main.py
@@ -0,0 +1,9 @@
+#Importamos los archivos  y lo agregamos a una lista
+USD_BLUE_FILE_PATH = './Data/usd_blue.xlsx'
+ALUA_FILE_PATH = './Data/alua.xlsx'
+BBAR_FILE_PATH = './Data/bbar.xlsx'
+BMA_FILE_PATH = './Data/bma.xlsx'
+
+XLSX_LIST = [USD_BLUE_FILE_PATH, ALUA_FILE_PATH, BMA_FILE_PATH, BBAR_FILE_PATH]
+
+print(XLSX_LIST)
