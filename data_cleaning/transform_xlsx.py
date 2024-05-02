c1251df2848711e78066fb48b600d35d8aaadfb4
Facu <facuovalles@gmail.com>
2024-05-02 12:25:56 -0300

    Nueva rama. Creando funciones de data_cleaning

---
+++ data_cleaning/transform_xlsx.py
@@ -0,0 +1,25 @@
+"""
+Este módulo proporciona funciones para transformar archivos XLSX en DataFrames.
+"""
+
+import pandas as pd
+import openpyxl
+
+def transform_xlsx(lista_xlsx):
+    """
+    Transformamos archivos XLSX  a dataframes y los almacena en una lista
+    Argumentos: 
+        lista_xlsx (list) : Lista con archivos XLSX
+    return:
+        lista de dataframes
+    """
+    df_list = []
+    for file in lista_xlsx:
+        libro = openpyxl.load_workbook(file)
+        hoja = libro.active
+        titulos = next(hoja.values)[0:]
+        df = pd.DataFrame(hoja.values, columns=titulos)
+        df = df.drop(0)
+        df_list.append(df)
+            
+    return df_list
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+"""
+Proyecto para calcular fuerza relativa entre acciones y dólar en Areentina
+"""
+
+from data_cleaning.transform_xlsx import transform_xlsx
 #Importamos los archivos  y lo agregamos a una lista
-USD_BLUE_FILE_PATH = './Data/usd_blue.xlsx'
-ALUA_FILE_PATH = './Data/alua.xlsx'
-BBAR_FILE_PATH = './Data/bbar.xlsx'
-BMA_FILE_PATH = './Data/bma.xlsx'
+
+USD_BLUE_FILE_PATH = './data/quotes/usd_blue.xlsx'
+ALUA_FILE_PATH = './data/quotes/alua.xlsx'
+BBAR_FILE_PATH = './data/quotes/bbar.xlsx'
+BMA_FILE_PATH = './data/quotes/bma.xlsx'
 
 XLSX_LIST = [USD_BLUE_FILE_PATH, ALUA_FILE_PATH, BMA_FILE_PATH, BBAR_FILE_PATH]
 
-print(XLSX_LIST)
+#Aplicamos una función para transformar la lista con arcvhivos xlsx en una lista con dataframes
+DF_LIST = transform_xlsx(XLSX_LIST)
+
+print(DF_LIST)
