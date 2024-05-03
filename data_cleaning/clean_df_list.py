058865e831c8bc9e25f17d13ff004dc81e5f6e5f
Facu <facuovalles@gmail.com>
2024-05-03 15:05:55 -0300

    Data cleaning. Optimizando modulo clean_df_list

--- data_cleaning/clean_df_list.py
+++ data_cleaning/clean_df_list.py
@@ -7,7 +7,7 @@ def clean_df_list(lista_df):
     Renombramos las columnas de la lista de dataframes y eliminamos la que no precisamos 
     Argumentos: 
         lista_df (list) : Lista con dataframes
-    Returns:
+    Return:
         lista con dataframes
     """
     for df in lista_df:
@@ -22,3 +22,26 @@ def clean_df_list(lista_df):
         df.drop(columns=columns_delete, inplace=True)
 
     return lista_df
+
+
+column_delete = ['Compra']
+
+def clean_df(df):
+    """
+    Renombramos las columnas dataframes y eliminamos la que no precisamos 
+    Argumentos: 
+        (df) : Dataframe
+    Return:
+        Dataframe
+    """
+
+
+    df.rename(columns={
+        'Fecha':'fecha',
+        'Venta':'cierre',
+        }, inplace=True  
+    )
+
+    df = df.drop(columns=column_delete, axis=1, inplace=True)
+
+    return df
--- main.py
+++ main.py
@@ -4,15 +4,15 @@ Proyecto para calcular fuerza relativa entre acciones y dólar en Argentina
 
 from data_cleaning.transform_xlsx import transform_xlsx
 from data_cleaning.clean_df_list import clean_df_list
+from data_cleaning.clean_df_list import clean_df
 from data_cleaning.clean_float_list import convert_float_list
 from data_cleaning.clean_date_list import convert_date_list
 
 #Importamos los archivos  y lo agregamos a una lista
-
 USD_BLUE_FILE_PATH = './data/quotes/usd_blue.xlsx'
 ALUA_FILE_PATH = './data/quotes/alua.xlsx'
-BBAR_FILE_PATH = './data/quotes/bbar.xlsx'
 BMA_FILE_PATH = './data/quotes/bma.xlsx'
+BBAR_FILE_PATH = './data/quotes/bbar.xlsx'
 
 XLSX_LIST = [USD_BLUE_FILE_PATH, ALUA_FILE_PATH, BMA_FILE_PATH, BBAR_FILE_PATH]
 
@@ -22,27 +22,8 @@ XLSX_SUBLIST = XLSX_LIST
 #Aplicamos una función para transformar la lista con arcvhivos xlsx en una lista con dataframes
 df_list = transform_xlsx(XLSX_SUBLIST)
 
-#Armamos un subset del usd, para mantener el original y modificar el subset
-df_usdb = df_list[0]
-
-#Cambiamos los nombres de la columa del DF_USDB que se descargó
-# de una fuente de datos distinta a la de las acciones.
-df_usdb.rename(columns={
-    'Fecha': 'fecha', 
-    'Venta': 'cierre'
-    },
-    inplace=True
-    )
-#Elimino la columna que no voy a utilizar
-df_usdb.drop("Compra", axis=1, inplace=True)
-
-#Como tengo DF_USDB por separado. Lo suelto de la lista de df
-df_list.pop(0)
-
-#Aplico la funcion de filtrado y renombre de columnas
-
-clean_df_list(df_list)
+#Aplico la funciones de data_cleaning
+clean_df(df_list[0])
+clean_df_list(df_list[1:])
 convert_float_list(df_list)
 convert_date_list(df_list)
-
-print(df_list[0])
